3f26104553879ac6959231ce9d858ba0095a2c68
Mel Boro <152180923+ItzSkyeYT@users.noreply.github.com>
2026-08-29 22:47:24 +0200

    Bump to 0.10.2 (79)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "dev.skye.intertune"
         minSdk = 24
         targetSdk = 36
-        versionCode = 78
-        versionName = "0.10.1-8"
+        versionCode = 79
+        versionName = "0.10.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
