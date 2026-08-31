cb47c080504b2218d66de8c755803c13274e17b0
Mel Boro <152180923+ItzSkyeYT@users.noreply.github.com>
2026-08-31 12:34:43 +0200

    Bump to 0.10.3 (80)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "dev.skye.intertune"
         minSdk = 24
         targetSdk = 36
-        versionCode = 79
-        versionName = "0.10.2"
+        versionCode = 80
+        versionName = "0.10.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
